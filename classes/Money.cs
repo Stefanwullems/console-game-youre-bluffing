e7bdef6531e6a4c5b90b0a5573d3dd6b751fe066
Stefanwullems <stefanwullems1998@gmail.com>
2018-12-03 19:52:25 +0100

    money logic implemented

--- classes/Money.cs
+++ classes/Money.cs
@@ -4,6 +4,20 @@ namespace youre_bluffing_console
 {
     class Money
     {
+        private static int[] donkeyBonus = new int[] { 50, 100, 200, 500 };
+        private int donkeyCount = 0;
 
+        public static int[] InitialHand()
+        {
+            int[] hand = new int[] { 10, 10, 10, 20, 50 };
+            return hand;
+        }
+
+        public int GetDonkeyBonus()
+        {
+            int bonus = donkeyBonus[donkeyCount];
+            donkeyCount++;
+            return bonus;
+        }
     }
 }
