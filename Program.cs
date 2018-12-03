848a7fc6847ac83b9a800c503ec34d423acb3cf0
Stefanwullems <stefanwullems1998@gmail.com>
2018-12-03 20:10:44 +0100

    tested player and bank

--- Program.cs
+++ Program.cs
@@ -8,9 +8,20 @@ namespace youre_bluffing_console
         static void Main(string[] args)
         {
             Animals animals = new Animals();
+            Bank bank = new Bank();
             for (int i = 0; i < animals.deck.Length; i++)
             {
                 Console.WriteLine(i.ToString() + " " + animals.deck[i] + " is valued at " + Animals.cardType[animals.deck[i]]);
+            }
+            Console.WriteLine(bank.GetDonkeyBonus().ToString());
+            Console.WriteLine(bank.GetDonkeyBonus().ToString());
+            Console.WriteLine(bank.GetDonkeyBonus().ToString());
+            Console.WriteLine(bank.GetDonkeyBonus().ToString());
+            Player frank = new Player();
+
+            for (int i = 0; i < frank.money.Length; i++)
+            {
+                Console.WriteLine(frank.money[i].ToString());
             }
         }
     }
