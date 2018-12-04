481a408ba67c889fdfec3b79fd155bf9f1f8e02f
Stefanwullems <stefanwullems1998@gmail.com>
2018-12-04 10:54:50 +0100

    added add animal and remove animal to player

--- classes/Player.cs
+++ classes/Player.cs
@@ -5,8 +5,11 @@ namespace youre_bluffing_console
     class Player
     {
         private int[] _money = new int[40];
+        private int moneyCount = 0;
         private string[] _animals = new string[40];
-        public string[] _quartets = new string[10];
+        private int animalCount = 0;
+        private string[] _quartets = new string[10];
+        private int quartetCount = 0;
         public Player()
         {
             _money = Bank.InitialHand();
@@ -25,6 +28,39 @@ namespace youre_bluffing_console
         public int[] GetMoney()
         {
             return _money;
+        }
+
+        public void AddAnimal(string card)
+        {
+            _animals[animalCount] = card;
+            int count = 0;
+            // Counts amount of animals of the same type in players hand
+            for (int i = 0; i < animalCount; i++) if (_animals[i] == card) count++;
+            //  If player has a quartet the animals get removed from the game and added to his quartets
+            if (count == 4)
+            {
+                for (int i = 0; i < count; i++) RemoveAnimal(card);
+                AddQuartet(card);
+            }
+
+        }
+
+        private void AddQuartet(string card)
+        {
+            _quartets[quartetCount] = card;
+        }
+
+        public void RemoveAnimal(string card)
+        {
+            animalCount--;
+            for (int i = 0; i < animalCount; i++)
+            {
+                if (_animals[i] == card)
+                {
+                    _animals[i] = _animals[animalCount];
+                    _animals[animalCount] = null;
+                }
+            }
         }
     }
 }
